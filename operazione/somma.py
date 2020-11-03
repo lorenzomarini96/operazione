e45acb209178df06c7a37fb5050cd07aa5f310b8
lorenzomarini96 <lorenzo.marini.1996@gmail.com>
2020-11-03 23:02:59 +0100

    Cambiato il docstrings per vedere se aggiorna la documentazione.

--- operazione/somma.py
+++ operazione/somma.py
@@ -1,9 +1,15 @@
+"""Programma che implementa la semplice operazione di somma tra due addendi (scalari).
+Non ha alcuna utilità pratica. Serve solo e soltanto per apprenderre il funzionamento della 
+documentazione con sphinx."""
+
 def somma(x, y):
-    """Funzione che somma due scalari e ne restituisce il risultato"""
+    """Funzione che somma due scalari e ne restituisce il risultato. x e y sono i valori 
+    dei due addendi."""
+
+
     return x + y
 
 
 
 
 
-    
